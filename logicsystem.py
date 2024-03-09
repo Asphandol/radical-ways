c9e430d377436ee925998ed5e70ae8be2b221b93
NazarM <143131471+Asphandol@users.noreply.github.com>
2024-03-09 20:13:48 +0200

    Add files via upload

---
+++ logicsystem.py
@@ -0,0 +1,165 @@
+"""
+logic system for our customers
+"""
+
+import random
+
+class LogicSystem:
+    """
+    a logic system class
+    """
+    __not_finished_orders = []
+
+    def delete_person(self):
+        """
+        delets person from app
+        """
+        pass
+
+    def change_info(self):
+        """
+        changes info about person
+        """
+        pass
+
+    def get_database(self):
+        """
+        gets database about persons
+        """
+        pass
+
+    def log_in(self):
+        """
+        logging in a person
+        """
+        pass
+
+    def sign_up(self):
+        """
+        helps a new person to sign up
+        """
+        pass
+
+    def sign_out(self):
+        """
+        helps tp sign out person
+        """
+        pass
+
+    def make_order(self):
+        """
+        makes an order 
+        """
+        pass
+
+    def show_map(self):
+        """
+        shows a map
+        """
+        pass
+
+class Person:
+    """
+    a person class
+    """
+    def __init__(self, name: str, surname: str, email_adress: str, password: str) -> None:
+
+        self.name = name
+        self.surname = surname
+        self.email_adress = email_adress
+        self.password = password
+        self.is_registred = True
+
+class User(Person):
+    """
+    user class
+    """
+
+    def __init__(self, name: str, surname: str, email_adress: str, password: str) -> None:
+
+        super().__init__(name, surname, email_adress, password)
+        self.is_driver = False
+
+    def make_request(self):
+        """
+        makes request
+        """
+        pass
+
+class Driver(Person):
+    """
+    driver class
+    """
+
+    def __init__(self, name: str, surname: str,
+                email_adress: str, password: str,
+                car: str, license_num: int) -> None:
+
+        super().__init__(name, surname, email_adress, password)
+
+        self.is_driver = True
+        self.car = car
+        self.license_num = license_num
+        self.is_able = True
+
+    def respond(self):
+        """
+        makes a respond on users rquest
+        """
+        pass
+
+    def finish_order(self):
+        """
+        finish users order
+        """
+        pass
+
+    def accept(self):
+        """
+        accept users request        
+        """
+        pass
+
+    def reject(self):
+        """
+        reject users request
+        """
+        pass
+
+    def end_session(self):
+        """
+        ends session with user
+        """
+        pass
+
+class Order:
+    """
+    creates an users order
+    """
+    def __init__(self) -> None:
+        self.is_finised = False
+        self.__id = random.randint(1, 999999)
+        self.__route = None
+
+class Map:
+    """
+    map class which
+    can help with
+    paving the way
+    """
+
+    def __init__(self, start: str, other_places: list) -> None:
+        self.start = start
+        self.other_places = other_places
+
+    def create_way(self):
+        """
+        creates the best way beetween multiple places
+        """
+        pass
+
+    def display_map(self):
+        """
+        helps with displaying map
+        """
+        pass
